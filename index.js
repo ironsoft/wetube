936cfaa5c55dd4e07d792455577e9fab7af72be1
ishopfloor <grismatin@naver.com>
2020-02-06 12:55:30 +0900

    first changes

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello!");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handlelistening = () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res){
+    res.send("Hello from home");
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handlelistening);
